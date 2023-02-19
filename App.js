189305135684561159145da7d9ea410168301cc8
Feras AlSouri <feras.el.souri@gmail.com>
2023-02-19 23:42:21 +0200

    first expo app

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class App extends React.Component {
+
+    state = {
+        text: "",
+        todo: ['this is list of todo', 'this second todo in the list']
+
+    }
+    addTodo = () => {
+        let text = this.state.text;
+        let arr = this.state.todo;
+        if (text !== "")
+            arr.push(text);
+        this.setState({todo: arr, text: ""});
+    }
+    deleteTodo = (t) => {
+        let arr = this.state.todo
+        let position = arr.indexOf(t)
+        arr.splice(position, 1);
+        this.setState({todo: arr})
+    }
+    renderTodoList = () => {
+        return this.state.todo.map((t) => {
+            return (
+                <TouchableOpacity key={t}>
+                    <Text
+                        style={styles.renderTodoList}
+                        onPress={() => this.deleteTodo(t)}
+                    >{t}</Text>
+                </TouchableOpacity>
+            )
+        })
+
+    }
+
+    render() {
+        return (
+            <View style={styles.viewStyles}>
+                <Text>This is new app </Text>
+                <TextInput
+                    style={styles.InputStyle}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                ></TextInput>
+                <Button
+                    title='ToDo+'
+                    onPress={this.addTodo}
+                ></Button>
+                {this.renderTodoList()}
+            </View>
+        );
+    }
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+    viewStyles: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+    InputStyle: {height: 40, borderWidth: 1, borderColor: "green", width: 300, marginTop: 10, marginBottom: 10},
+    renderTodoList: {marginTop: 10}
+}
+
+export default App;
